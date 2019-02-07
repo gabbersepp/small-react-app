de3a335c1459e0c74693c05651d1f70a456fe904
Josef Biehler <jbiehler@samhammer.de>
2019-02-07 16:41:14 +0100

    add electron

--- src/index.js
+++ src/index.js
@@ -3,6 +3,13 @@ import ReactDOM from "react-dom";
 import App from "./components/App";
 import "./style/index.css";
 
+const e = document.getElementById("root");
+if (!e) {
+    let root = document.createElement('div');
+    root.id = "root";
+    document.body.appendChild( root );
+}
+
 ReactDOM.render(
     <App/>,
     document.getElementById("root")
