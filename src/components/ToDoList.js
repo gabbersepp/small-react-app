f1788802d21f94138fcd6e5f42189400e80faf25
Josef Biehler <jbiehler@samhammer.de>
2019-02-07 15:04:53 +0100

    fix todo input

--- src/components/ToDoList.js
+++ src/components/ToDoList.js
@@ -6,19 +6,22 @@ import "./../style/todo.css";
 export default class ToDoList extends Component {
     constructor(props) {
         super(props);
-        this.todos = [{ title: "Todo 1" }, { title: "Todo 2"}];
+        this.state = {
+            todos: [{ title: "Todo 1" }, { title: "Todo 2"}]
+        }
         this.input = this.input.bind(this);
     }
 
     input(value) {
-        this.todos.push({title: value});
+        this.state.todos.push({title: value});
+        this.setState(this.state); // very important! use setState() instead of direct assignment!
     }
 
     render() {
         return (
             <div id="todo-list">
                 <ToDoInput input={this.input}/>
-                {this.todos.map(todo => (<div className="todo-entry">{todo.title}</div>))}
+                {this.state.todos.map(todo => (<div className="todo-entry">{todo.title}</div>))}
             </div>
         )
     }
