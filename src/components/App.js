dc4830c36071ee680592b36568fb188d609bd9d4
Josef Biehler <jbiehler@samhammer.de>
2019-02-27 11:52:40 +0100

    add router

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,33 @@
 import React, { Component } from "react";
 import ToDoList from "./ToDoList";
 import LivePreview from "./LivePreview";
+import HomePage from "./HomePage";
+import { Route, Redirect } from "react-router";
+import { HashRouter, Link } from "react-router-dom";
 
 class App extends Component {
     render() {
         return (
-            <div>
-                <ToDoList/>
-                <LivePreview/>
-            </div>
+            <HashRouter>
+                <div>
+                    <nav>
+                        <Link to="/todos">ToDos</Link>
+                        <Link to="/">Home</Link>
+                    </nav>
+                    
+                    <Route path="/todos" exact={true} render={() => (
+                        <div>
+                        <ToDoList/>
+                        <LivePreview/>
+                        </div>
+                    )}/>
+                    <Route path="/home" exact={true} component={HomePage}/>
+                    <Route path="/" exact={true} render={() => (
+                        <Redirect to="/home"/>
+                    )}/>
+                    
+                </div>
+            </HashRouter>
         )
     }
 }
---
+++ src/components/HomePage.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class HomePage extends Component {
+    render() {
+        return (
+            <div style={{textAlign: "center"}}>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto obcaecati tenetur est quam, excepturi nam laborum incidunt similique eum perferendis esse quibusdam sunt porro? Pariatur, nam? Nisi mollitia placeat recusandae.
+            </div>
+        )
+    }
+}
